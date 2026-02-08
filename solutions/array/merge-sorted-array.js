9a45885866dede047bf3737ae35c495eeac99c92
luutanhung <luutanhung.dev@gmail.com>
2026-02-08 08:39:48 +0700

    docs: add problem description for 88. Merge Sorted Array

--- solutions/array/merge-sorted-array.js
+++ solutions/array/merge-sorted-array.js
@@ -1,4 +1,16 @@
 /**
+ * 88. Merge Sorted Array
+ *
+ * Problem:
+ * Give two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Exzmple:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * Output:
+ * [1,2,2,3,5,6]
+ *
  * @param {number[]} nums1
  * @param {number} m
  * @param {number[]} nums2
