ddc3d74c9d7cfbdb97bdf5ff4726e74c28e82ebc
luutanhung <luutanhung.dev@gmail.com>
2026-02-08 08:59:57 +0700

    fix: typo in docs 88. Merge Sorted Array

--- solutions/array/merge-sorted-array.js
+++ solutions/array/merge-sorted-array.js
@@ -4,7 +4,7 @@
  * Problem:
  * Give two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
  *
- * Exzmple:
+ * Example:
  * nums1 = [1,2,3,0,0,0], m = 3
  * nums2 = [2,5,6],       n = 3
  *
