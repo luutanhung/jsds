d2fb77b693e1ba2e9e951e1385b15528ee75f032
luutanhung <luutanhung.dev@gmail.com>
2026-02-08 16:44:36 +0700

    feat: add initial version of MPromise

---
+++ mechanics/mpromise.js
@@ -0,0 +1,79 @@
+const MPromiseState = Object.freeze({
+	Pending: "PENDING",
+	Fulfilled: "Fulfilled",
+	Rejected: "Rejected",
+});
+
+class MPromise {
+	constructor(executorFunc) {
+		this.state = MPromiseState.Pending; // "pending" | "fulfilled" | "rejected"
+		this.value = undefined;
+		this.handlers = [];
+
+		const resolve = (val) => {
+			if (this.state !== "pending") return;
+			this.state = MPromiseState.Fulfilled;
+			this.value = val;
+			this.handlers.forEach((h) => h?.onFulfilled?.(val));
+		};
+
+		const reject = (err) => {
+			if (this.state !== MPromise.Pending) return;
+			this.state = MPromiseState.Rejected;
+			this.value = err;
+			this.handlers.forEach((h) => h?.onRejected?.(err));
+		};
+
+		if (typeof executorFunc === "function") {
+			executorFunc(resolve, reject);
+		}
+	}
+
+	then(onFulfilled, onRejected) {
+		if (this.state === MPromiseState.Fulfilled) {
+			onFulfilled?.(this.value);
+		} else if (this.state === MPromiseState.Rejected) {
+			onRejected?.(this.value);
+		} else {
+			this.handlers.push({
+				onFulfilled,
+				onRejected,
+			});
+		}
+
+		return this;
+	}
+
+	catch(onRejected) {
+		return this.then(null, onRejected);
+	}
+
+	static resolve(val) {
+		const p = new MPromise();
+		p.state = MPromiseState.Fulfilled;
+		p.value = val;
+		return p;
+	}
+
+	static reject(err) {
+		const p = new MPromise();
+		p.state = MPromiseState.Rejected;
+		p.value = err;
+		return p;
+	}
+}
+
+function test() {
+	return new MPromise((resolve) => {
+		setTimeout(() => {
+			resolve("Done.");
+		}, 1000);
+	});
+}
+
+async function run() {
+	const result = await test();
+	console.log(result);
+}
+
+run();
